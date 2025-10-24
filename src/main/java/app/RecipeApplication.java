c27c3238b65d07563cf427364a422e536e2747d8
EmiAba <106099092+EmiAba@users.noreply.github.com>
2025-10-24 11:39:22 +0300

    refactor: RecipeApplication

--- src/main/java/app/RecipeApplication.java
+++ src/main/java/app/RecipeApplication.java
@@ -4,11 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableAsync
 @EnableScheduling
 @EnableCaching
 @EnableFeignClients
