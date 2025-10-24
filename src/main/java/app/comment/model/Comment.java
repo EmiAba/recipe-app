4e79a01d47302f43661260f3b2863a05cc08ccba
EmiAba <106099092+EmiAba@users.noreply.github.com>
2025-10-24 11:32:26 +0300

    refactor: update Comment model structure

--- src/main/java/app/comment/model/Comment.java
+++ src/main/java/app/comment/model/Comment.java
@@ -30,11 +30,11 @@ public class Comment {
     @Column(nullable = false)
     private LocalDateTime updatedOn;
 
-    @ManyToOne(fetch = FetchType.EAGER)
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id", nullable = false)
     private User author;
 
-    @ManyToOne(fetch = FetchType.EAGER)
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "recipe_id", nullable = false)
     private Recipe recipe;
 }
