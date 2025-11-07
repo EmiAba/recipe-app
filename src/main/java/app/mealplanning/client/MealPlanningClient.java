8f6df6f70040326d3a12c033e41ae6aedf3c79d3
EmiAba <106099092+EmiAba@users.noreply.github.com>
2025-11-07 12:23:50 +0200

    feat: Add meal planning Feign client

---
+++ src/main/java/app/mealplanning/client/MealPlanningClient.java
@@ -0,0 +1,24 @@
+package app.mealplanning.client;
+
+
+import app.mealplanning.client.dto.MealPlanResponse;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.UUID;
+
+@FeignClient(name = "meal-planning-svc", url = "http://localhost:8081/api/v1/meal-plans")
+public interface MealPlanningClient {
+
+
+    @GetMapping("/weekly")
+    ResponseEntity<List<MealPlanResponse>> getWeeklyMealPlans(
+            @RequestParam("userId") UUID userId,
+            @RequestParam("weekStart") LocalDate weekStart
+    );
+
+}
